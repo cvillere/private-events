90744a9012b4656192a026c54175caf1b6f1446e
Christian <christianvillere@gmail.com>
2024-07-13 18:46:29 -0700

    Fix routing and error for user show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  devise_scope :user do
+    get 'users/:id', to: 'users/sessions#show', as: 'user'
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,8 +19,8 @@ Rails.application.routes.draw do
   resources :users
   resources :event_attendees, only: [:new, :create]
   resources :events do
-    post 'attend' => 'event_attendees#create'
-    delete 'unattend' => 'event_attendees#destory'
+    #post 'attend' => 'event_attendees#create'
+    #delete 'unattend' => 'event_attendees#destory'
   end
 
 
