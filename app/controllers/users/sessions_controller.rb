cd7c19d4154a47f0f3f9e066f59f307abe9c5255
Christian <christianvillere@gmail.com>
2024-07-21 17:47:27 -0700

    Change User show page

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,31 +1,32 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  #before_action :configure_sign_in_params, only: [:create, :show]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  #def new
+    #super
+  #end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  #def create
+    #super
+  #end
 
   def show
+    @events = current_user.events
     @user = User.find(params[:id])
   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  #def destroy
+    #super
+  #end
 
   # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  #If you have extra params to permit, append them to the sanitizer.
+  #def configure_sign_in_params
+     #devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  #end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
+
   }
 
   devise_scope :user do
     get 'users/:id', to: 'users/sessions#show', as: 'user'
+    #get 'users/sign_out' => "devise/sessions#destroy"
+    get 'users/sign_in', to: 'devise/sessions#new'
+
   end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
